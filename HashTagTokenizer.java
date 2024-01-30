ec8c94480a6ec64415d3ad82c126f40efb9a9bf5
Maya Kenigsberg <maya.kenigsberg@post.runi.ac.il>
2024-01-30 19:18:06 +0200

    first attempt

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -4,23 +4,39 @@ public class HashTagTokenizer {
 
 	public static void main(String[] args) {
 
-		String hashTag = args[0];
+		String hashTag = args [0];
 		String []dictionary = readDictionary("dictionary.txt");
 		breakHashTag(hashTag, dictionary);
 	}
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
 		In in = new In(fileName);
-
-		// Your code here
-
+		for (int i=0;i<dictionary.length;i++)
+		{
+			dictionary[i]= in.readLine();
+		}
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+		for (int i=0;i<dictionary.length;i++)
+		{
+			if (word.length()== dictionary[i].length())
+			{
+				boolean innerCheck = true;
+				for(int j=0; j<word.length();j++)
+				{
+					if (word.charAt(j)!=dictionary[i].charAt(j))
+					{
+						innerCheck=false;
+					}
+				}
+				if (innerCheck)
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
@@ -29,12 +45,27 @@ public class HashTagTokenizer {
         if (hashtag.isEmpty()) {
             return;
         }
- 
         int N = hashtag.length();
+		boolean innerCheck = false;// checks to see if a word was found within the hashtag
+		int newword = 0;
+		hashtag=hashtag.toLowerCase();
+        for (int i = 1; i <= N; i++) 
+		{
+			String sub =hashtag.substring(0, i);
+			if (existInDictionary(sub, dictionary))
+			{
+				System.out.println(sub);
+				 newword= i;
+				 innerCheck=true;
+			}	
 
-        for (int i = 1; i <= N; i++) {
-		
         }
+		if(innerCheck)// if a word was found, preform the function again on the remaining hashtag
+			{
+				breakHashTag(hashtag.substring(newword), dictionary);
+			}
+		else
+			return;
     }
-
-}
+	
+ }
